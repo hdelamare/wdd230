e6c84578a4a7bf2c975ea727c65652ee65ae87b3
hdelamare <82909829+hdelamare@users.noreply.github.com>
2021-05-13 13:47:19 -0500

    added new js

---
+++ lesson4/js/currentYear.js
@@ -0,0 +1,3 @@
+const d1 = new Date();
+const currYear = d1.getFullYear();
+document.querySelector('#currYear').textContent = currYear;
--- lesson4/js/lastModified.js
+++
@@ -1,6 +0,0 @@
-let date = new Date();
-let currYear = date.getFullYear();
-document.querySelector('#currYear').textContent = currYear;
-
-let modifiedTime = document.lastModified;
-document.querySelector('#modTime').textContent = modifiedTime;
