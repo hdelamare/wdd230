d617a1a77aa362b2f474ee03c0bd193ff8676492
hdelamare <82909829+hdelamare@users.noreply.github.com>
2021-06-24 05:32:32 -0500

    weather api added to preston

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,19 +1,53 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=4525e1da6c6e94ce8e10f5f9d9d3db01"
-fetch(apiURL)
+const apiWeatherURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=4525e1da6c6e94ce8e10f5f9d9d3db01"
+fetch(apiWeatherURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
+    document.getElementById('weather-description').textContent = jsObject.weather[0].main;
+    document.getElementById('temp').textContent = jsObject.main.temp.toFixed(0);
+    let temp = jsObject.main.temp
+    let speed = jsObject.wind.speed
+    windChill(temp, speed)
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('speed').textContent = jsObject.wind.speed.toFixed(0);
   });
 
 const apiForecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=4525e1da6c6e94ce8e10f5f9d9d3db01"
 fetch(apiForecastURL)
     .then((response) => response.json())
-    .then((jsObject) => {
-        console.log(jsObject);
-    });
+    .then((forecast) => {
+        console.log(forecast);
+
+        for (let i in forecast.list) {
+          if (forecast.list[i].dt_txt.includes("18:00:00")) {
+            let forecastBox = document.createElement('div');
+            forecastBox.setAttribute('class', 'forecast');
+
+            let forecastDay = document.createElement('div');
+            forecastDay.setAttribute('class', 'forecast-day');
+            
+            forecastWeekDay = new Date(forecast.list[i].dt_txt);
+            forecastDayName = forecastWeekDay.getDay();
+            forecastDay.textContent = days[forecastDayName].substring(0, 3);
+            
+            forecastBox.appendChild(forecastDay);
+
+            let forecastImg = document.createElement('img');
+            const forecastImgSrc = 'https://openweathermap.org/img/wn/' + forecast.list[i].weather[0].icon + '@2x.png';
+            const imgAltDesc = forecast.list[i].weather[0].description;
+
+            forecastImg.setAttribute('class', 'forecast-img');
+            forecastImg.setAttribute('src', forecastImgSrc);
+            forecastImg.setAttribute('alt', imgAltDesc);
+            
+            forecastBox.appendChild(forecastImg);
+
+            let forecastTemp = document.createElement('p');
+            forecastTemp.setAttribute('class', 'forecast-temp');
+
+            forecastTemp.textContent = forecast.list[i].main.temp_max.toFixed(0) + '°F';
+
+            forecastBox.appendChild(forecastTemp);
+
+            document.querySelector('.forecast-div').appendChild(forecastBox); 
+    }}});
--- lesson10/js/windChill.js
+++ lesson10/js/windChill.js
@@ -1,16 +1,10 @@
-let temp = document.querySelector('#temp').innerHTML;
-let speed = document.querySelector('#speed').innerHTML;
-
-if ((temp <= 50) && (speed > 3)) {    
-    let result = windChill(temp, speed)
-    document.querySelector('#windchill').innerHTML = result.toFixed(0) + '°F';
-}
-
-else {
-    document.querySelector('#windchill').innerHTML = 'N/A';
-}
-
 function windChill(temp, speed) {
-    let f = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
-    return f
-}
+
+    if ((temp <= 50) && (speed > 3)) {    
+        let f = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+        document.querySelector('#windchill').innerHTML = f.toFixed(0);
+    }
+
+    else {
+        document.querySelector('#windchill').innerHTML = 'N/A';
+    }};
